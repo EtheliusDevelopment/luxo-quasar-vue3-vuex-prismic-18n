b27188b91cb1e34df09b1db2a8118aac2439d4c7
EtheliusDevelopment <etheliusdev@gmail.com>
2021-07-20 08:18:39 +0100

    api spinner test

--- src/components/Prismic/testComponentPrismic.vue
+++ src/components/Prismic/testComponentPrismic.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Here's your raw API response:</h1>
+    <q-spinner-clock v-if="spinner!=false" color="teal" size="4rem" />
     <pre id="app" v-html="JSON.stringify(response, null, 2)"></pre>
   </div>
 </template>
@@ -11,19 +12,32 @@ export default {
   data() {
     return {
       // Initialize "response"
-      response: null
+      response: null,
+      spinner: false
     }
   },
   methods: {
     async getContent() {
+
       // Query the API and assign the response to "response"
       const response = await this.$prismic.client.query('')
-      this.response = response.results;
+      this.response = response.results[0].data;
+
+
+
     }
   },
   created() {
     // Call the API query method
     this.getContent()
+    this.spinner = true;
+    console.log(this.spinner)
+      setTimeout(()=>{
+        this.spinner = false;
+        console.log(this.spinner)
+      },3000);
+
+
   }
 }
 </script>
