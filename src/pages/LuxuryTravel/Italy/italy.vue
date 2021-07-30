6dc5cb8caf4807b3beea9727f1ed51df8a0ab624
EtheliusDevelopment <etheliusdev@gmail.com>
2021-07-30 21:35:38 +0100

    preloader working all app dynamic pàges

--- src/pages/LuxuryTravel/Italy/italy.vue
+++ src/pages/LuxuryTravel/Italy/italy.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page class="flex flex-center" padding>
+    <PreLoader />
     <DynamicContent />
   </q-page>
 </template>
 
 <script>
+import PreLoader from 'src/components/PreLoader.vue';
 import DynamicContent from "../../../components/LuxuryTravel/DynamicContent.vue";
 export default {
   components: {
     DynamicContent,
+    PreLoader,
   },
 
   setup() {},
