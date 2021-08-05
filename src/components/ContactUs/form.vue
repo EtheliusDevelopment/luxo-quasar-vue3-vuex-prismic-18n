b326e12fa3fd501f4c7d591c8f183973bdff2560
EtheliusDevelopment <etheliusdev@gmail.com>
2021-08-05 13:22:48 +0100

    grid system completed

---
+++ src/components/ContactUs/form.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+            <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+
+      </div>
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const $q = useQuasar()
+
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+
+    return {
+      name,
+      age,
+      accept,
+
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+        }
+      }
+    }
+  }
+}
+</script>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -3,70 +3,90 @@
     <PreLoader />
 
     <div class="header-block">
-        <q-img
-          class="img1"
-          img-class="img1-luxury"
-          src="~assets/umbria_luxo.jpg"
-          :ratio="16 / 9"
-          spinner-color="primary"
-          spinner-size="82px"
-          height="450px"
+      <q-img
+        class="img1"
+        img-class="img1-luxury"
+        src="~assets/umbria_luxo.jpg"
+        :ratio="16 / 9"
+        spinner-color="primary"
+        spinner-size="82px"
+        height="450px"
+      >
+        <div class="figcaption">
+          <h6 class="text-white">LUXURY TRAVEL DESIGNERS</h6>
+          <q-separator color="white" inset />
+          <h1 class="text-white text-center">PAUL &amp; ANDREA</h1>
+          <q-btn color="white" icon="check" outline label="LEARN WHAT WE DO" />
+        </div>
+      </q-img>
+
+      <div class="breadcrumbs">
+        <q-breadcrumbs
+          separator="---"
+          class="text-orange"
+          active-color="primary"
         >
-          <div class="figcaption">
-            <h6 class="text-white">LUXURY TRAVEL DESIGNERS</h6>
-            <q-separator color="white" inset />
-            <h1 class="text-white text-center">PAUL &amp; ANDREA</h1>
-            <q-btn
-              color="white"
-              icon="check"
-              outline
-              label="LEARN WHAT WE DO"
-            />
-          </div>
-        </q-img>
-
-        <div class="breadcrumbs">
-          <q-breadcrumbs
-            separator="---"
-            class="text-orange"
-            active-color="primary"
-          >
-            <q-breadcrumbs-el icon="home" />
-            <q-breadcrumbs-el label="Components" icon="widgets" to="/" />
-            <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" />
-          </q-breadcrumbs>
-        </div>
-      </div>
-
-    <div class="first-block" style="outline: dashed">
-      <div class="sub-section-first">
-
-        <div class="info-component">
-         <h3>INFO COMOPNENT</h3>
-        </div>
-
-        <div class="form-component">
-         <h3>FORM COMOPNENT</h3>
-        </div>
-
-        <div class="contact-card-component">
-          <h3>CONTACT CARD COMPONENT</h3>
-        </div>
-
+          <q-breadcrumbs-el icon="home" />
+          <q-breadcrumbs-el label="Components" icon="widgets" to="/" />
+          <q-breadcrumbs-el label="Breadcrumbs" icon="navigation" />
+        </q-breadcrumbs>
       </div>
     </div>
 
+    <div class="first-block" style="outline: dashed">
+      <div class="info-component">
+        <p>
+          No matter what your interests and passions, no matter what the season,
+          we can bring your perfect trip to life. Simply tell us what you’re
+          into, share with us the real reason for your trip, and we’ll bring
+          your vision to life.
+        </p>
 
+        <h6 class="text-primary">The Process:</h6>
+
+        <p>
+          <span class="text-info"> Step 1:</span> You tell us what you like, why
+          you’re travelling and what you absolutely have to see and do.
+        </p>
+
+        <p>
+          <span class="text-info"> Step 2:</span> We analyse your needs and
+          compile an itinerary that is perfectly “you”.
+        </p>
+        <p>
+          <span class="text-info"> Step 3:</span> Together we review your custom
+          itinerary and fine-tune the details.
+        </p>
+        <p>
+          <span class="text-info"> Step 4:</span> We lock the dates down and
+          you’re ready to travel – it’s that simple!
+        </p>
+      </div>
+
+      <div class="contact-card-component bg-info">
+        <div class="inner-component">
+          <h2 class="text-primary">Luxo Italia</h2>
+          <p>Via Galla Placidia 12 - Milano, Italy</p>
+          <p>Email : info@luxoitalia.com</p>
+        </div>
+      </div>
+
+      <div class="form-component">
+        <Form />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import PreLoader from "src/components/PreLoader.vue";
 import { ref } from "vue";
+import Form from "../components/ContactUs/form.vue";
 
 export default {
   components: {
     PreLoader,
+    Form,
   },
   setup() {
     return {
@@ -78,15 +98,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
 // **********HEADER BLOCK**********
 @media screen and (max-width: 680px) {
-
-  .img1{
+  .img1 {
     height: 15% !important;
   }
-
 }
 
 @media screen and (max-width: 990px) {
@@ -116,8 +132,6 @@ hr {
   margin-bottom: 2vh;
 }
 
-
-
 // **********FIRST BLOCK**********
 @media screen and (max-width: 990px) {
 }
@@ -126,7 +140,24 @@ hr {
 }
 
 .first-block {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 40% 25% auto;
+  grid-gap: 2%;
+  margin: 0 5%;
 }
 
+.info-component {
+  grid-column: 1;
+  grid-row: 1;
+}
 
+.contact-card-component {
+  grid-column: 1;
+  grid-row: 2;
+}
+.form {
+  grid-column: 2;
+  grid-row: 1/4;
+}
 </style>
